a44bbdd53b6c91f2eba7cd423fca6e6a439c8029
‘husnamaan21’ <husnamaan1999@gmail.com>
2021-05-20 11:04:05 -0700

    Update app.js
    CHANGES

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
 
 function addItem(){
-    var toDo=document.getElementById("todo");
- 
-
+  var app=document.getElementById("todo")
+  var toDo=app.value;
+  if(toDo.length == 0 || toDo.length == null){
+    alert("invalid length")
+  }else{
     // create li
     var li=document.createElement('li');
-    var liText=document.createTextNode(toDo.value);
+    var liText=document.createTextNode(toDo);
     li.appendChild(liText);
     var list=document.getElementById("list");
+     //create edit button
     var btn1= document.createElement('button');
      var btnText1=document.createTextNode('Edit');
-    // set function on del button
+    // set function on edit button
     btn1.setAttribute("onclick","edit(this)")
     btn1.setAttribute("class","jsbtn")
     btn1.setAttribute("id","btn1")
     btn1.style.color="green";
     btn1 .appendChild(btnText1);
-    //append del button with li
+  
+
+    //append edit button with li
     li.appendChild(btn1);
     //create del button
      var btn= document.createElement('button');
@@ -31,21 +36,37 @@ function addItem(){
     btn.style.color="red";
     //append del button with li
     li.appendChild(btn);
-     //create edit button
+    
      
       //append li  with  list
     list.appendChild(li);
-    toDo.value="";
+    app.value="";
 }
+
+}
+  
+   // del item   
 function del(e){
-    console.log(e)
+ 
+    e.parentNode.remove();
+
 }
+// del all items
 function delItem(){
   var ul=document.getElementById("list");
   ul.innerHTML="";
 }
+
+// edit item
 function edit(e){
-var p=e.parentNode.firstChild.nodeValue;
-var edit=prompt("edit your name",p);
-e.parentNode.firstChild.nodeValue=edit;
+var val=e.parentNode.firstChild.nodeValue;
+console.log(val)
+var edit=prompt("edit your name",val);
+
+if(edit.length == 0 || edit.length == null){
+  alert("invalid length")
+}else{
+  e.parentNode.firstChild.nodeValue=edit;
+}
+
 }
